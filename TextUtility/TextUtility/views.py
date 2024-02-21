bd8239b1d3525500d9451f6b99d1c512ff423d21
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2024-02-21 18:01:39 +0530

    converted all the GET requests to POST so that the data is passed in the request body instead of URL

--- TextUtility/TextUtility/views.py
+++ TextUtility/TextUtility/views.py
@@ -5,27 +5,27 @@ def home(request):
     return render(request,"home.html")
 
 def capitalize(request):
-    print("\n\n",request.GET.get("input_text","default"))
-    received_text = request.GET.get("input_text","default") # collect the data that is passed to input_text i.e. textarea
+    print("\n\n",request.POST.get("input_text"))
+    received_text = request.POST.get("input_text") # collect the data that is passed to input_text i.e. textarea
     modified_text = received_text.capitalize()
     print("MODIFIED TEXT",modified_text)
     params = {'modified_text':modified_text}
     return render(request,"capitalize.html",params)
 
 def upper(request):
-    received_text = request.GET.get('input_text')
+    received_text = request.GET.get("input_text")
     modified_text = received_text.upper()
     params = {"modified_text":modified_text}
     return render(request,'upper.html',params)
 
 def lower(request):
-    received_text = request.GET.get('input_text')
+    received_text = request.GET.get("input_text")
     modified_text = received_text.lower()
     params = {"modified_text":modified_text}
     return render(request,'lowercase.html',params)
 
 def trim(request):
-    received_text = request.GET.get('input_text')
+    received_text = request.GET.get("input_text")
     modified_text = received_text.replace(" ","")
     params = {"modified_text":modified_text}
     return render(request,'trim.html',params)
